00158b1fe40d87b73f2b8f297caf8050056e4dae
Stéphane Toyo Demanou <stephanetoyo14@gmail.com>
2025-01-17 20:12:02 -0500

    Added modules, components and services to client

---
+++ client/src/app/pages/signup-page/signup-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-page',
+  imports: [],
+  templateUrl: './signup-page.component.html',
+  styleUrl: './signup-page.component.scss'
+})
+export class SignupPageComponent {
+
+}
