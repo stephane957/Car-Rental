fd3b66e22883565e25020562536d89e8a4ac5e01
Stéphane Toyo Demanou <stephanetoyo14@gmail.com>
2025-01-17 21:27:30 -0500

    Added basic routes on the client
    Deleted the placeholder code in app.component.html

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomePageComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'register', component: SignupPageComponent },
+    { path: 'login',component: LoginPageComponent },
+];
